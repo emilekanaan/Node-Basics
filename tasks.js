14d39814098c9d6338dae006bd8a46ea02ccce3d
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-23 10:08:21 +0200

    added done property to tasks

--- tasks.js
+++ tasks.js
@@ -1,65 +1,59 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
 function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------------------------");
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-
-  if (text === 'quit\n' || text === 'exit\n') {
+  if (text === "quit\n" || text === "exit\n") {
     quit();
-  }
-  else if (text.startsWith("hello")) {
+  } else if (text.startsWith("hello")) {
     hello(text);
-  } else if (text === 'help\n') {
+  } else if (text === "help\n") {
     help();
-  } else if (text === 'list\n') {
-    list()
-  } else if (text === 'add\n' || text === 'add \n') {
-    console.log("please type what you want to add after the add command")
+  } else if (text === "list\n") {
+    list();
+  } else if (text === "add\n" || text === "add \n") {
+    console.log("please type what you want to add after the add command");
   } else if (text.startsWith("add") && text.length > 4) {
-    add(text)
+    add(text);
   } else if (text.startsWith("remove")) {
-    remove(text)
+    remove(text);
   } else if (text.startsWith("edit")) {
-    edit(text)
-  }
-  else {
+    edit(text);
+  } else {
     unknownCommand(text);
   }
 }
 
-let list1 = ["get potato", "get bread", "get tomato"]
-
+let list1 = ["[ ] get potato", "[\u2713] get bread", "[\u2713] get tomato"];
 
 /**
  * prints "unknown command"
@@ -69,52 +63,59 @@ let list1 = ["get potato", "get bread", "get tomato"]
  * @returns {void}
  */
 function unknownCommand(c) {
-  console.log('unknown command: "' + c.trim() + '"')
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
- * 
+ *
  * @param {string} c the text received
  * @returns {void}
  */
 function hello(c) {
-  console.log(`${c.trim()}!`)
+  console.log(`${c.trim()}!`);
 }
 
 /**
  * add items to the list
- * 
+ *
  * @param {string} c the text received
  * @returns {void}
  */
 function add(c) {
   console.log(`adding: "${c.slice(4, c.length - 1)}"...`);
-  list1.push(c.slice(4, c.length - 1));
+  list1.push(`[ ] ${c.slice(4, c.length - 1)}`);
   console.log(`added!`);
 }
 
 /**
  * remove the last item from the list
  * and if you add it with a number it will remove the item of that number
- * 
+ *
  * @param {string} c the text received
  * @returns {void}
  */
 function remove(c) {
   if (c.length <= 7) {
-    console.log("removing the last element!")
-    list1.pop();
-    console.log("last element removed!")
+    if (list1.length === 0) {
+      console.log(`nothing to remove, the list is empty!`);
+    } else {
+      console.log("removing the last element...");
+      list1.pop();
+      console.log("last element removed!");
+    }
   } else {
     let index = c.slice(7);
-    if (index.slice(0, index.length - 1) > list1.length || index.slice(0, index.length - 1) < 1 || isNaN(index.slice(0, index.length - 1))) {
-      console.log(`item ${index.slice(0, index.length - 1)} does not exist!`)
+    if (
+      index.slice(0, index.length - 1) > list1.length ||
+      index.slice(0, index.length - 1) < 1 ||
+      isNaN(index.slice(0, index.length - 1))
+    ) {
+      console.log(`item ${index.slice(0, index.length - 1)} does not exist!`);
     } else {
-      console.log(`removing element: ${index.slice(0, index.length - 1)}`)
+      console.log(`removing element: ${index.slice(0, index.length - 1)}`);
       list1.splice(index - 1, 1);
-      console.log(`element ${index.slice(0, index.length - 1)} removed!`)
+      console.log(`element ${index.slice(0, index.length - 1)} removed!`);
     }
   }
 }
@@ -122,60 +123,87 @@ function remove(c) {
 /**
  * edit the last item from the list
  * and if you add it with a number it will edit the item of that number
- * 
+ *
  * @param {string} c the text received
  * @returns {void}
  */
 function edit(c) {
   if (c.length <= 6) {
-    console.log("Error!\nPlease enter what you want to edit...\ntype help command for more informations");
+    console.log(
+      "Error!\nPlease enter what you want to edit...\ntype help command for more informations"
+    );
   } else {
-    let index = parseInt(c.slice(5))
-    if (isNaN(index)) {
-      let element = c.slice(5)
-      console.log(`editing "${list1[list1.length - 1]}" to "${element.slice(0, element.length - 1)}"...`)
-      list1[list1.length - 1] = element;
-      console.log(`editing completed!`)
-    } else if (index > list1.length || index < 1) {
-      console.log(`item ${index} does not exist`)
+    if (list1.length === 0) {
+      console.log(`nothing to edit, the list is empty!`);
     } else {
-      let element = c.slice(6 + index.toString().length)
-      console.log(`editing "${list1[index - 1]}" to "${element.slice(0, element.length - 1)}"...`)
-      list1[index - 1] = element.slice(0, element.length - 1);
-      console.log(`editing completed!`)
+      let index = parseInt(c.slice(5));
+      if (isNaN(index)) {
+        let element = c.slice(5);
+        if (list1[list1.length - 1].slice(0, 3) === "[ ]") {
+          console.log(
+            `editing "${list1[list1.length - 1].slice(4)}" to "${element.slice(
+              0,
+              element.length - 1
+            )}"...`
+          );
+          list1[list1.length - 1] = `[ ] ${element.slice(0, element.length - 1)}`;
+          console.log(`editing completed!`);
+        } else {
+          console.log(
+            `editing "${list1[list1.length - 1].slice(4)}" to "${element.slice(
+              0,
+              element.length - 1
+            )}"...`
+          );
+          list1[list1.length - 1] = `[\u2713] ${element.slice(0, element.length - 1)}`;
+          console.log(`editing completed!`);
+        }
+      } else if (index > list1.length || index < 1) {
+        console.log(`item ${index} does not exist`);
+      } else {
+        let element = c.slice(6 + index.toString().length);
+        if (list1[index - 1].slice(0, 3) === "[ ]") {
+          console.log(
+            `editing "${list1[index - 1].slice(4)}" to "${element.slice(
+              0,
+              element.length - 1
+            )}"...`
+          );
+          list1[index - 1] = `[ ] ${element.slice(0, element.length - 1)}`;
+          console.log(`editing completed!`);
+        } else {
+          console.log(
+            `editing "${list1[index - 1].slice(4)}" to "${element.slice(
+              0,
+              element.length - 1
+            )}"...`
+          );
+          list1[index - 1] = `[\u2713] ${element.slice(0, element.length - 1)}`;
+          console.log(`editing completed!`);
+        }
+      }
     }
-
   }
-  // if (c.length <= 5) {
-  //   console.log("editing the last element!")
-  //   list1.pop();
-  //   console.log("last element editd!")
-  // } else {
-  //   let index = c.slice(5);
-  //   if (index.slice(0, index.length - 1) > list1.length || index.slice(0, index.length - 1) < 1 || isNaN(index.slice(0, index.length - 1))) {
-  //     console.log(`item ${index.slice(0, index.length - 1)} does not exist!`)
-  //   } else {
-  //     console.log(`editing element: ${index.slice(0, index.length - 1)}`)
-  //     list1.splice(index - 1, 1);
-  //     console.log(`element ${index.slice(0, index.length - 1)} editd!`)
-  //   }
-  // }
 }
 
 /**
  * lists the items in the list
- * 
- * 
+ *
+ *
  * @returns {void}
  */
 function list() {
-  console.log(`here is the list:`);
-  list1.map((item, index) => {
-    index++;
-    console.log(`${index} - ${item}`);
-  })
+  console.log("[\u2713] or [ ]");
+  if (list1.length === 0) {
+    console.log(`the list is empty!`);
+  } else {
+    console.log(`here is the list:`);
+    list1.map((item, index) => {
+      index++;
+      console.log(`${index} - ${item}`);
+    });
+  }
 }
-
 
 /**
  * prints all the possible commands
@@ -184,14 +212,16 @@ function list() {
  * @returns {void}
  */
 function help() {
-  console.log("these are the commands:\n" +
+  console.log(
+    "these are the commands:\n" +
     " + help : this command will list all the commands for you.\n" +
     " + hello : this command greets you, you have to type hello and then your name (ex: 'hello Emile' will return 'hello Emile!') so it will greet you with your name, and it can greet you without typing your name also (ex: 'hello' will return 'hello!'\n" +
     " + quit or exit : this command exit the application.\n" +
     " + add : this command add items to the list.\n" +
     " + remove : this command removes the last item from the list and if you add after it a number (ex: 'remove 3') it will remove the number of element from the list.\n" +
-    " + list : this command list all the items in the list.\n"
-  )
+    " + list : this command list all the items in the list.\n" +
+    " + edit : this command will edit the items on the list (ex: 'edit new text' should change the last task to 'new text') or (ex: 'edit 1 new text' should change the task 1 to 'new text')."
+  );
 }
 
 /**
@@ -200,9 +230,9 @@ function help() {
  * @returns {void}
  */
 function quit() {
-  console.log('Quitting now, goodbye!')
+  console.log("Quitting now, goodbye!");
   process.exit();
 }
 
 // The following line starts the application
-startApp("Emile Kanaan")
+startApp("Emile Kanaan");
